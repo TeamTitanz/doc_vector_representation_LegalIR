beef67346cb879a061082687aa6ba33005db30bc
Buddhi Ayesha <buddhiayesha.13@cse.mrt.ac.lk>
2017-06-19 15:07:12 +0530

    Adding shell script

---
+++ DocVec.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+java -Xmx5g -jar TF-IDF-1.0-SNAPSHOT-jar-with-dependencies.jar Cases Serialized_folder
+
+
+#!/bin/bash
+#module load java
+#java -Xmx5g -jar ./LawIE/DocVector/TF-IDF-1.0-SNAPSHOT-jar-with-dependencies.jar ./LawIE/DocVector/Cases ./LawIE/DocVector/Serialized_folder
